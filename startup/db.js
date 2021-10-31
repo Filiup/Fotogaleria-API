2a425b68b188b6a5387dca239c37b7811ede22c0
Filiup <filip.barabas@student.spseke.sk>
2021-10-31 21:07:43 +0100

    propojenie sa k MongoDB

---
+++ startup/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const winston = require('winston');
+
+module.exports = () => {
+    mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => winston.info('Connected to MongoDB...'))
+  
+    mongoose.set('useFindAndModify', false);
+  
+    // Zakážeme, aby driver mnogoose menil mená kolekcií automaticky do plurálu (Mohlo by to robiť problémi )
+    // Chceme aby meno vytvorenej kolekcie bolo rovnaké ako názov galérie
+    mongoose.pluralize(null);
+  
+  
+};
