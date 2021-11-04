43496aefba1ad9cdf497190259fe6b2b0d29d286
Filiup <filip.barabas@student.spseke.sk>
2021-11-04 13:06:59 +0100

    refactor, odstranenie zbytocnych funkcii

--- others/previewImage.js
+++ others/previewImage.js
@@ -3,64 +3,29 @@ const { Gallery } = require('../models/gallery');
 async function deletePreviewImage(galleryName) {
     // Pomocou metódy updateOne zmeníme v dokumente s daným menom hodnotu "image" na null
     await Gallery.updateOne({name: galleryName}, {
-        $set: {
-            image: null
+        $unset: {
+            preview: 1
         }
     });
 
 }
 
-/*
-async function updatePreviewImage(gallery, imageModel) {
-    // Z databázy vytiahneme všetky obrázky pre daný ImageModel a potom z nich vyberieme len ten prvý ( Prvý obrázok bude vždy náhľadový )
-    const images = await imageModel.find().lean().select("-_id -__v"); 
-    const previewImage = images[0];
-  
-    // Pokiaľ daná galéria obsahuje aspoň jeden obrázok, tak zmeníme jej náhľadový obrázok 
-    if (images.length) {
-        await Gallery.findByIdAndUpdate(gallery._id, {
-            image: {
-                path: previewImage.path,
-                fullpath: previewImage.fullpath,
-                name: previewImage.name,
-                uploaded: previewImage.uploaded
-            }
-        }, { new: true });
-    
-    }
-  
-  };
-*/
-async function updatePreviewImage(GalleryName, image) {
-    await Gallery.findOneAndUpdate({name : GalleryName}, {
-        preview: {
+
+async function updatePreviewImage(galleryName, image) {
+    if (!image) return;
+    await Gallery.findOneAndUpdate({name : galleryName}, {
+        preview:  {
             path: image.path,
             fullpath: image.fullpath,
             name: image.name,
             uploaded: image.uploaded
-        }
+        } 
     });
 };
   
-async function resetPreviewImage(galleryModel, galleryName, imageDocument, imageModel) {
-    const GalleryPreview = galleryModel.image;
-  
-    // Pokiaľ sa meno náhľadového obrázku v galérii rovná menu obrázku v jej kolekcii, ktorý ideme vymazať, tak to znamená, že náhľadový obrázok bol vymazaný
-  
-    if (GalleryPreview.path === imageDocument.path) {
-        // Zmažeme náhľadový obrázok
-        deletePreviewImage(galleryName);
-        
-        // Zavoláme funkciu updatePreviewImage kvôli tomu, aby sa zmenil náhľadový obrázok na iný
-        // ( pokiaľ kolekcia galérie obsahuje ešte nejaké fotky, pokiaľ nie tak ostane hodnota null )
-        updatePreviewImage(galleryModel, imageModel);
-  
-  
-    };
-  
-}
+
 
 module.exports.updatePreviewImage = updatePreviewImage;
-module.exports.resetPreviewImage = resetPreviewImage;
+module.exports.deletePreviewImage = deletePreviewImage;
 
   
