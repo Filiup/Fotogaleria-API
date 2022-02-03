ea4e8fd9da348edd9ba1080b6d435b025ac9f27b
Filiup <filip.barabas@student.spseke.sk>
2022-02-03 17:38:05 +0100

    formatovanie kodu

--- startup/db.js
+++ startup/db.js
@@ -1,10 +1,13 @@
-const mongoose = require('mongoose');
-const winston = require('winston');
+const mongoose = require("mongoose");
+const winston = require("winston");
 
-module.exports = () => {
-    mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => winston.info('Connected to MongoDB...'))
-  
-    mongoose.set('useFindAndModify', false);
+module.exports = () => {
+  mongoose
+    .connect(process.env.MONGO_DB, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => winston.info("Connected to MongoDB..."));
 
+  mongoose.set("useFindAndModify", false);
 };
